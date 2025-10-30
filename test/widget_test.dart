a043e68af8091af275b69b46d9d277fc0181cf27
jeremiah chinedu <chinedujeremiah2002@gmail.com>
2025-10-30 15:18:55 +0100

    add README.md file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive/hive.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:personal_task_manager/features/task/application/task_provider.dart';
 import 'package:personal_task_manager/features/task/data/task_repository.dart';
 import 'package:personal_task_manager/main.dart';
-import 'package:personal_task_manager/models/task.dart';
 import 'package:personal_task_manager/services/hive_service.dart';
 
 import 'widget_test.mocks.dart';
